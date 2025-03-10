8859a9f18e56898fcff4df6986087bdfa6293c6d
RaffaeleSergio <raffaele16590@gmail.com>
2025-03-10 19:14:52 +0100

    feat: inizio sconto

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 preventForm = document.getElementById("prevent");
-const backendSelect = "backend"
-const frontendSelect = "frontend"
-const projectSelect = "project"
-const sumP = document.getElementById("sum")
-const sconto =["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
+const backendSelect = "backend";
+const frontendSelect = "frontend";
+const projectSelect = "project";
+const sumP = document.getElementById("sum");
+const codeInput = document.getElementById("code");
+const sconto = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
 let workSelect = document.getElementById("work").value;
 
 preventForm.addEventListener("submit", gestisciForm);
+
+for (let i = 0; i < sconto.length; i++) {
+    const curSconto = sconto[i];
+
+
+}
+
+
 
 
 function gestisciForm(event) {
@@ -24,9 +33,11 @@ function gestisciForm(event) {
         prezzo = 10 * 20.50
     }
 
+    let prezzoScontato = codeInput.value;
     
-const tot = prezzo.toFixed(2);
-sumP.innerText = "€ " + tot;
+
+    const tot = prezzo.toFixed(2);
+    sumP.innerHTML = `<i class="bi bi-currency-euro"></i> ${tot}`
 
 }
 
